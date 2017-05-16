2580781a9ca6b2f57403503c981c72e2f7c7fc55
Makhles <mr.lange85@gmail.com>
2017-05-16 10:45:21 -0300

    Project created with a simple test example

---
+++ src/testes/TesteExemplo.java
@@ -0,0 +1,14 @@
+package testes;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class TesteExemplo {
+
+	@Test
+	public void teste() throws Exception {
+		assertTrue(true);
+	}
+}
