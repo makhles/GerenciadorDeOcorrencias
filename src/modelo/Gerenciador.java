05ae86043b9aa17db8461dee02141fa94f91426c
Makhles <mr.lange85@gmail.com>
2017-05-23 08:42:43 -0300

    Teste do cadastro de funcionários sem nenhum argumento corrigido.

--- src/modelo/Gerenciador.java
+++ src/modelo/Gerenciador.java
@@ -5,16 +5,22 @@ import java.util.List;
 
 public class Gerenciador {
 
+	private List<Funcionario> funcionarios;
+
+	public Gerenciador() {
+		funcionarios = new ArrayList<Funcionario>();
+	}
+
 	public List<Funcionario> obterFuncionarios() {
 		return new ArrayList<Funcionario>();
 	}
 
 	public void cadastrarFuncionario() {
-		
+		funcionarios.add(new Funcionario());
 	}
 
 	public int obterQuantidadeDeFuncionarios() {
-		return 0;
+		return funcionarios.size();
 	}
 
 }
