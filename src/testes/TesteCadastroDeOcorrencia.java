a3c021f6fd668794254b2a25bfd6daada836cc75
Makhles <mr.lange85@gmail.com>
2017-05-30 09:50:54 -0300

    Teste de cadastro de uma ocorrência em um projeto não cadastrado.

--- src/testes/TesteCadastroDeOcorrencia.java
+++ src/testes/TesteCadastroDeOcorrencia.java
@@ -13,6 +13,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import excecoes.ExcecaoOcorrenciaComIdJaCadastrada;
+import excecoes.ExcecaoProjetoNaoCadastrado;
 
 public class TesteCadastroDeOcorrencia {
 
@@ -41,13 +42,24 @@ public class TesteCadastroDeOcorrencia {
 		assertEquals(superBug, gerenciador.obterProjetoComId(1).obterOcorrencias().get(0));
 	}
 
-//	@Test(expected = ExcecaoOcorrenciaComIdJaCadastrada.class)
-//	public void testeCadastrarOcorrenciaComIdJaCadastrado() throws Exception {
-//		final String RESUMO = "TL;DR";
-//		Funcionario bob = new Funcionario(1, "Bob");
-//		Ocorrencia superBug = new Ocorrencia(1, bob, TipoOcorrencia.BUG, Prioridade.ALTA, RESUMO);
-//		Ocorrencia minorBug = new Ocorrencia(1, bob, TipoOcorrencia.BUG, Prioridade.BAIXA, RESUMO);
-//		gerenciador.cadastrarOcorrencia(superBug);
-//		gerenciador.cadastrarOcorrencia(minorBug);
-//	}
+	@Test(expected = ExcecaoOcorrenciaComIdJaCadastrada.class)
+	public void testeCadastrarOcorrenciaComIdJaCadastrado() throws Exception {
+		final String RESUMO = "TL;DR";
+		Funcionario bob = new Funcionario(1, "Bob");
+		Ocorrencia superBug = new Ocorrencia(1, bob, TipoOcorrencia.BUG, Prioridade.ALTA, RESUMO);
+		Ocorrencia minorBug = new Ocorrencia(1, bob, TipoOcorrencia.BUG, Prioridade.BAIXA, RESUMO);
+		gerenciador.cadastrarOcorrencia(superBug, manhattan);
+		gerenciador.cadastrarOcorrencia(minorBug, manhattan);
+	}
+
+	@Test(expected = ExcecaoProjetoNaoCadastrado.class)
+	public void testeCadastrarOcorrenciaEmProjetoNaoCadastrado() throws Exception {
+		final String RESUMO = "TL;DR";
+		Funcionario bob = new Funcionario(1, "Bob");
+		Ocorrencia superBug = new Ocorrencia(1, bob, TipoOcorrencia.BUG, Prioridade.ALTA, RESUMO);
+		Ocorrencia minorBug = new Ocorrencia(1, bob, TipoOcorrencia.BUG, Prioridade.BAIXA, RESUMO);
+		Projeto newOrder = new Projeto(2, "NewOrder");
+		gerenciador.cadastrarOcorrencia(superBug, newOrder);
+		gerenciador.cadastrarOcorrencia(minorBug, newOrder);
+	}
 }
